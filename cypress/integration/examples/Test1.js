8cba865262db2a3840f05ab5b3f325825ad498ca
celestialheathen <jingchaoyu@hotmail.com>
2021-08-02 17:19:01 -0400

    added get, wait and assertion

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,13 @@
+/// <reference types="Cypress" />
+
 describe('My first test', function() {
   it('Does not do much', function() {
     
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length', 4)
 
   })
 })
