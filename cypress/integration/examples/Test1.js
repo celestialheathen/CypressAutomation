73931a9379f4c3ee322951c502472f7bf25dd63c
celestialheathen <jingchaoyu@hotmail.com>
2021-08-03 17:08:32 -0400

    added clicks and each method

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -7,7 +7,19 @@ describe('My first test', function() {
 
     cy.get('.search-keyword').type('ca')
     cy.wait(2000)
+
     cy.get('.product:visible').should('have.length', 4)
+
+    cy.get('.products').find('.product').should('have.length', 4)
+
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+    cy.get('.products').find('.product').each(($el, index, $list) => {
+      const textVeg = $el.find('h4.product-name').text()
+      if (textVeg.includes('Cashews')) {
+        $el.find('button').click()
+      }
+    })
 
   })
 })
